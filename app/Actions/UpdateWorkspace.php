28ace4ed73d480bd7941ec3cbb6c8dc22210d2f8
Hashane <jhashane@gmail.com>
2026-07-01 17:44:11 +0300

    feat: Update workspace action

--- app/Actions/UpdateWorkspace.php
+++ app/Actions/UpdateWorkspace.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace App\Actions;
 
-use App\Models\User;
 use App\Models\Workspace;
 
 final readonly class UpdateWorkspace
 {
-    public function handle(User $user,int $id, string $name): Workspace
+    public function handle(Workspace $workspace, string $name): void
     {
-        return $user->workspaces()->update(id,[
+        $workspace->update([
             'name' => $name,
         ]);
     }
