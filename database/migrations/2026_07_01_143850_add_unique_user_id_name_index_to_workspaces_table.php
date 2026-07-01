1d25e7af82bf47c5fc921caa34139341d01206de
Hashane <jhashane@gmail.com>
2026-07-01 17:49:20 +0300

    Feat: Workspace update

---
+++ database/migrations/2026_07_01_143850_add_unique_user_id_name_index_to_workspaces_table.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('workspaces', function (Blueprint $table): void {
+            $table->unique(['user_id', 'name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('workspaces', function (Blueprint $table): void {
+            $table->dropUnique(['user_id', 'name']);
+        });
+    }
+};
